7a91a5ce415e613dfc466c5d4ec52451e6b2b93e
eai6 <edwardamoahidun@gmail.com>
2023-11-28 19:53:00 -0500

    inference with docker

--- dbh_estimation_algorithm_FastAPI/README.md
+++ dbh_estimation_algorithm_FastAPI/README.md
@@ -4,3 +4,16 @@ Source code for tree dbh esimtaiton algorithm
 docker buildx build --platform=linux/amd64 -t pv_mrv_api:x86 .
 docker tag pv_mrv_api:x86 eai6/pv_mrv_api:x86  
 docker push eai6/pv_mrv_api:x86 
+
+
+
+# Manual refresh
+pip install --upgrade pip
+pip install tensorflow==2.13.0rc0
+pip install pandas
+pip install "fastapi[all]"
+pip install imutils
+pip install opencv-python
+pip install Pillow
+pip install matplotlib
+pip install statsmodels
--- dbh_estimation_algorithm_FastAPI/app/scripts/runTilles.py
+++ dbh_estimation_algorithm_FastAPI/app/scripts/runTilles.py
@@ -1,12 +1,116 @@
-from image_slicer import Tile, slice as slice_image
-from scripts import deeplab_model 
+#from image_slicer import Tile, slice as slice_image
+from app.scripts import deeplab_model 
 from PIL import Image
 import numpy as np
 from numpy import asarray
 import os
 
+
+
+def slice_image(input_image_path, output_directory, num_tiles):
+    """
+    Slice the input image into the specified number of tiles.
+
+    Parameters:
+    - input_image_path: Path to the input image file.
+    - output_directory: Directory to save the sliced tiles.
+    - num_tiles: Number of tiles to create.
+
+    Returns:
+    - List of paths to the sliced tiles.
+    """
+    # get extension of input image
+    extension = os.path.splitext(input_image_path)[1]
+    
+    # Open the input image
+    img = Image.open(input_image_path)
+
+    # Create output directory if it doesn't exist
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # Get image dimensions
+    width, height = img.size
+
+    # Calculate tile size
+    tile_width = width // num_tiles
+    tile_height = height // num_tiles
+
+    # List to store paths of sliced tiles
+    sliced_tiles = []
+
+    # Iterate over rows and columns to slice the image
+    for i in range(num_tiles):
+        for j in range(num_tiles):
+            # Calculate coordinates for cropping each tile
+            left = j * tile_width
+            upper = i * tile_height
+            right = left + tile_width
+            lower = upper + tile_height
+
+            # Crop the tile
+            tile = img.crop((left, upper, right, lower))
+
+            # Save the tile to the output directory
+            tile_path = os.path.join(output_directory, f"tile_{i}_{j}{extension}")
+            tile.save(tile_path)
+
+            # Append the path to the list
+            sliced_tiles.append(tile_path)
+
+    return sliced_tiles
+
+
+
+def combine_tiles(tiles_directory, output_image_path, num_tiles):
+    """
+    Combine sliced tiles into a single image.
+
+    Parameters:
+    - tiles_directory: Directory containing the sliced tiles.
+    - output_image_path: Path to save the combined image.
+    - num_tiles: Number of tiles per row and column.
+
+    Returns:
+    - Path to the combined image.
+    """
+    # Create a list to store the tiles
+    tiles = []
+
+    # get extension of input image
+    extension = os.listdir(tiles_directory)[0].split('.')[-1]
+    #print(extension)
+
+    # Load each tile and append to the list
+    for i in range(num_tiles):
+        for j in range(num_tiles):
+            tile_path = os.path.join(tiles_directory, f"tile_{i}_{j}.{extension}")
+            tile = Image.open(tile_path)
+            tiles.append(tile)
+
+    # Calculate the dimensions of the combined image
+    tile_width, tile_height = tiles[0].size
+    total_width = tile_width * num_tiles
+    total_height = tile_height * num_tiles
+
+    # Create a new image with the calculated dimensions
+    combined_image = Image.new("RGB", (total_width, total_height))
+
+    # Paste each tile onto the combined image
+    for i in range(num_tiles):
+        for j in range(num_tiles):
+            left = j * tile_width
+            upper = i * tile_height
+            combined_image.paste(tiles[i * num_tiles + j], (left, upper))
+
+    # Save the combined image
+    combined_image.save(output_image_path)
+
+    return output_image_path
+
+
 # load image
-def runTile(filename, tile_number):
+def runTile(filename, i, j):
     im = Image.open(filename)
 
     # run model 
@@ -15,66 +119,38 @@ def runTile(filename, tile_number):
 
     # move to background later -- saved mask
     new_seg_iamge = Image.fromarray(np.uint8(seg_image)).convert('RGB')
-    new_seg_iamge.save(f'app/data/outputs/temp_tile_{tile_number}.png')
-    resized_img.save(f'app/data/outputs/resized_img_{tile_number}.png')
+    new_seg_iamge.save(f'app/data/mask_tiles/tile_{i}_{j}.png')
+    resized_img.save(f'app/data/resized_tiles/tile_{i}_{j}.png')
 
-    return [f'app/data/outputs/temp_tile_{tile_number}.png', f'app/data/outputs/resized_img_{tile_number}.png']
+    return [f'app/data/mask_tiles/tile_{i}_{j}.png', 'app/data/resized_tiles/tile_{i}_{j}.png']
 
-
-def stitch_tiles_together(tile_paths) -> Image.Image:
-        """Stitch the tiles back together."""
-
-        # get the dimensions of each tile
-        with Image.open(tile_paths[0]) as mask_tile_image:
-            mask_tile_width = mask_tile_image.width
-            mask_tile_height = mask_tile_image.height
-
-        # new full-size segmentation mask with the same dimensions as original image
-        image = Image.new('RGB', (mask_tile_width*3, mask_tile_height*3), (0, 0, 0))
-
-        # Combine segmentation mask tiles back together
-        row = 0
-        col = 0
-        for p in tile_paths:
-            with Image.open(p) as image_tile:
-                #print(image_tile.size)
-
-                #detected_class_indices: List[int] = np.unique(image).tolist()
-                #print(f"tile: {p} has classes: {detected_class_indices}")
-
-                # determine where in the full segmentation mask the tile should go
-                offset = mask_tile_width * col, mask_tile_height * row
-
-                image.paste(image_tile, offset)
-
-                # update row/col value based on where previous tile was pasted
-                if col != 2:
-                    col += 1
-                else:
-                    row += 1
-                    col = 0
-
-        return image
 
 
 def runTilles(path):
+    # get file extension
+    extension = os.path.splitext(path)[1]
+    # reference to the tiles
+    tiles_path = "app/data/tiles"
     # tile the image
-    tiles= slice_image(path, 9)
+    tile_num = 3
+    tiles = slice_image(path, tiles_path , tile_num)
 
     # run model on tiles
     mask_paths = []
     resized_img_paths = []
-    for i in range(len(tiles)):
-        mask_file, img_file = runTile(tiles[i].filename, i)
-        mask_paths.append(mask_file)
-        resized_img_paths.append(img_file)
+    for i in range(tile_num):
+        for j in range(tile_num):
+            mask_file, img_file = runTile( f"{tiles_path}/tile_{i}_{j}{extension}", i,j)
+            mask_paths.append(mask_file)
+            resized_img_paths.append(img_file)
 
     # put mask together
-    seg_image = stitch_tiles_together(mask_paths)
+    seg_image = combine_tiles("app/data/mask_tiles", "app/data/outputs/seg_image.png", tile_num)
+    seg_image = Image.open("app/data/outputs/seg_image.png")
 
     # stich resized images together
-    resized_img = stitch_tiles_together(resized_img_paths)
-    resized_img.save('app/data/outputs/resized_img.png')
+    resized_img = combine_tiles("app/data/resized_tiles", "app/data/outputs/resized_img.png", tile_num)
+    resized_img = Image.open("app/data/outputs/resized_img.png")
 
     # convert image into numpy arrray
     seg_image = asarray(seg_image)
@@ -85,12 +161,9 @@ def runTilles(path):
             os.remove(mask)
         except:
             continue
-    
-    for tile in tiles:
-        try:
-            os.remove(tile.filename)
-        except:
-            continue
+
+
+
 
     # return seg_image
     return resized_img, seg_image
